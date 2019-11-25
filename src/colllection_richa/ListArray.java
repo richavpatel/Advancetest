e0ceacf114d6187237138fd5d07514e7b1c77749
RichaVPatel <richu2593@gmail.com>
2019-11-24 19:53:56 -0500

    Added ListArray and Solutions

---
+++ src/colllection_richa/ListArray.java
@@ -0,0 +1,39 @@
+package colllection_richa;
+// duplicate elements in ArrayList
+// how to get unique String using hashset from duplicate arraylist 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ListArray {
+
+	public static void main(String[] args) {
+		List<String> uniqueList = new ArrayList<>();
+
+		uniqueList.add("Richa");
+		uniqueList.add("Roma");
+		uniqueList.add("Dhruvesh");
+		uniqueList.add("Richa");
+
+		Set<String> u = new HashSet<>();
+
+		for (String s : uniqueList) {
+
+			
+			  if (u.add(s) == false) { //if i do true then i will get unqiue set!!
+				  System.out.println("Find duplicate string from hash set: " +s);
+				  }
+			  }
+			 
+
+			/*if (u.contains(s)) {
+				System.out.println("Find duplicate string from hash set: "+ s);
+			}
+			u.add(s);*/
+
+		//}
+
+	}
+
+}
---
+++ src/colllection_richa/Solutions.java
@@ -0,0 +1,57 @@
+package colllection_richa;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solutions {
+	
+   public static void main(String[] args) {
+		
+		
+		int[] nums = {2,7,1,6};
+		int target = 9;
+			
+		System.out.println("Using HashMap Index value of sum of two Int : " + Arrays.toString(twoSum(nums, target)));
+		
+	Map<Integer, Integer> numMap = new HashMap<>();
+	numMap.put(1, 10);
+	numMap.put(2, 20);
+	numMap.get(1);
+	//System.out.println("richa tst" +numMap.containsKey(7));
+	//System.out.println("richa tst" +numMap.get(1));
+		
+		for(int i =0; i<nums.length; i++) {
+			numMap.put(nums[i], i);  
+			// System.out.println(nums[i]);//loop thi index ni value malse
+			
+		}
+		
+	}
+	public static int[] twoSum(int[] nums, int target) {
+		
+		Map<Integer, Integer> numMap = new HashMap<>();
+		
+		for(int i =0; i<nums.length; i++) {
+			numMap.put(nums[i], i);          //loop thi index ni value malse
+			
+		}
+		
+		for(int i= 0; i<nums.length; i++) {
+			
+			int numToFind = target - nums[i];//9-2 = 7
+		   if ( numMap.containsKey(numToFind) && numMap.get(numToFind) != i) {
+			   return new int[] {i, numMap.get(numToFind)};
+		   }
+			
+			
+			
+		}
+		return new int[0]; 
+	//	throw new IllegalArgumentException("S");
+		
+		
+	}
+
+}
